bc7f2ad4588f6cff9938d73da48cb3d44abeee0f
YongsuKim <jzqioipst@daum.net>
2020-06-26 21:55:00 +0900

    update quiz5_1_fibonacci.py

--- quiz/quiz5_1_fibonacci.py
+++ quiz/quiz5_1_fibonacci.py
@@ -3,11 +3,18 @@
 """
 
 
-def fibonacci(n):
+def fibonacci(n, memo=None):
+    if memo is None:
+        memo = {}
+
     if n <= 2:
         return 1
 
-    return fibonacci(n - 2) + fibonacci(n - 1)
+    if n in memo:
+        return memo[n]
+
+    memo[n] = fibonacci(n - 1, memo) + fibonacci(n - 2, memo)
+    return memo[n]
 
 
 print(fibonacci(10))
